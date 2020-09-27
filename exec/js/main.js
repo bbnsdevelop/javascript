b1c6139e8fac298f6e2ccbef8d7f2efffe77c096
bbnsdevelop <bbnsdevelop@gmail.com>
2020-09-26 21:03:23 -0300

    html e js básico

---
+++ exec/js/main.js
@@ -0,0 +1,23 @@
+// variaveis
+
+var nome = "Mestre";
+var sobreNome = "Jedi";
+
+
+function exibirNome(){
+    console.log(`Nome é: ${nome} ${sobreNome}`);
+}
+
+
+function exibirFuncao(){
+    console.log(`v2 - Nome é: ${nome} ${sobreNome}`);
+}
+
+
+function eventClick(){
+    alert(`Nome é: ${nome} ${sobreNome}`);
+}
+
+
+// chamada de funções
+exibirNome();
