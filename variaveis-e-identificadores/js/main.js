ef29e9f114da8809292e5303aec7b08ae48f55bd
bbnsdevelop <bbnsdevelop@gmail.com>
2020-09-29 23:40:22 -0300

    criação de variaveis

---
+++ variaveis-e-identificadores/js/main.js
@@ -0,0 +1,11 @@
+//variaveis
+var nome;
+var idade = 20;
+nome = "Bruno";
+var pretensaoSalarial = 277.22
+var valeRefeicao = true;
+
+
+function eventOnClick(){
+    alert(`Nome programador ${nome} idade ${idade} salário do programador ${pretensaoSalarial} tem vale refeição? ${valeRefeicao}`)
+}
