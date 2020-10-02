40c710e2a2dfd9609cf508588b6e4ca946163244
bbnsdevelop <bbnsdevelop@gmail.com>
2020-10-01 21:57:30 -0300

    tipo de dados String

---
+++ tipos-de-dados/js/main.js
@@ -0,0 +1,14 @@
+//variaveis
+var nome = "Bruno";
+var sobreNome = "Santos";
+var nomeCompleto = `${nome} ${sobreNome}`;
+
+
+console.log(`Nome completo: ${nomeCompleto}`);
+
+var num1 = 20 + "10";
+var num2 = 20;
+
+console.log(` tipo de dados 1 ${typeof num1}`)
+console.log(` tipo de dados 2 ${typeof num2}`)
+console.log(` tamanho da string ${num1.length}`)
--- variaveis-e-identificadores/js/main.js
+++ variaveis-e-identificadores/js/main.js
@@ -5,7 +5,14 @@ nome = "Bruno";
 var pretensaoSalarial = 277.22
 var valeRefeicao = true;
 
+var pessoa = {
+    nome: "Bruno",
+    idade: 20,
+    pretensaoSalarial: 77.22,
+    valeRefeicao: true
+
+}
 
 function eventOnClick(){
-    alert(`Nome programador ${nome} idade ${idade} salário do programador ${pretensaoSalarial} tem vale refeição? ${valeRefeicao}`)
+    alert(`Nome programador ${pessoa.nome} idade ${pessoa.idade} salário do programador R$ ${pessoa.pretensaoSalarial} tem vale refeição? ${pessoa.valeRefeicao}`)
 }
