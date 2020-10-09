2f58bde563664571f7911c99c34a7fb9b09de481
bbnsdevelop <bbnsdevelop@gmail.com>
2020-10-08 22:27:32 -0300

    operadores de comparação

--- operadores/operadores.js
+++ operadores/operadores.js
@@ -10,10 +10,19 @@ console.log(`${mult}`)
 let divisao = 25 / 5;
 console.log(`${divisao}`)
 
-let resto = 26 % 2;
-console.log(`${resto}`)
+let modulo = 26 % 2;
+console.log(`${modulo}`)
 
 let OperadorAtribuicao = 10;
 
 console.log(`comparacao verifica o valor é ? ${OperadorAtribuicao == "10"}`)
 console.log(`comparacao verifica o valor e o tipo é ? ${OperadorAtribuicao === "10"}`)
+console.log(`comparacao diferente valor é ? ${OperadorAtribuicao != 10}`)
+console.log(`comparacao diferente valor e tipo é ? ${OperadorAtribuicao !== "10"}`)
+console.log(`comparacao maior é ? ${OperadorAtribuicao > 10}`)
+console.log(`comparacao menor é ? ${OperadorAtribuicao < 10}`)
+console.log(`comparacao maior ou igual é ? ${OperadorAtribuicao >= 10}`)
+console.log(`comparacao menor ou igual é ? ${OperadorAtribuicao <= 10}`)
+
+let exp = 2 ** 3;
+console.log(`${exp}`)
