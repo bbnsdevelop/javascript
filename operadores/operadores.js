f6be52d8056d6f8cb82d8c5ab9b4241664f22749
bbnsdevelop <bbnsdevelop@gmail.com>
2020-10-08 21:11:56 -0300

    operadores

---
+++ operadores/operadores.js
@@ -0,0 +1,19 @@
+let soma = 1 + 3;
+console.log(`${soma}`)
+
+let subtracao = 10 - 4;
+console.log(`${subtracao}`)
+
+let mult = 5 * 6;
+console.log(`${mult}`)
+
+let divisao = 25 / 5;
+console.log(`${divisao}`)
+
+let resto = 26 % 2;
+console.log(`${resto}`)
+
+let OperadorAtribuicao = 10;
+
+console.log(`comparacao verifica o valor é ? ${OperadorAtribuicao == "10"}`)
+console.log(`comparacao verifica o valor e o tipo é ? ${OperadorAtribuicao === "10"}`)
