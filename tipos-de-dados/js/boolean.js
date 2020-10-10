95a1540d52899e21fcd3324daccf35f11211158f
bbnsdevelop <bbnsdevelop@gmail.com>
2020-10-09 23:33:57 -0300

    conhecendo melhor funcionalidade de String, Boolean e Number

--- tipos-de-dados/js/boolean.js
+++ tipos-de-dados/js/boolean.js
@@ -3,3 +3,16 @@ var isVerdadeiro = true;
 if(isVerdadeiro){
     console.log(isVerdadeiro);
 }
+
+
+var a = true;//literal
+console.log(a, "tipo: ", typeof a);
+
+var b = new Boolean(false);
+console.log(b, "tipo: ", typeof b);
+console.log(b.toString(), "tipo: ", typeof b.toString());
+
+//retornar o valor primitivo
+console.log("valueOf", b.valueOf(), typeof b.valueOf());
+console.log(b.toString().toUpperCase());
+console.log(b.toString().length);
