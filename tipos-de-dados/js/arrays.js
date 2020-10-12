5f19e4ff03c9c2995b3b93b6bb49d580aee27e90
bbnsdevelop <bbnsdevelop@gmail.com>
2020-10-12 12:57:00 -0300

    verificar se um index existe no array

--- tipos-de-dados/js/arrays.js
+++ tipos-de-dados/js/arrays.js
@@ -9,6 +9,11 @@ console.log('------------------------')
 let arrayConstrutor = new Array(23, 25, 26, 29);
 showArray(arrayConstrutor);
 
+// verificar se existe o index dentro do array
+
+let existe = new Boolean( 10 in arrayConstrutor);
+
+console.log(`Existe? ${ existe == true ? "Existe":"Não existe" }`);
 
 function showArray(array) {
     for (let i = 0; i < array.length; i++) {
