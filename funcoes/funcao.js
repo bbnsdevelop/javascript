843e38a07804a37e3760a8478e121e7f3e92adb6
bbnsdevelop <bbnsdevelop@gmail.com>
2020-10-15 23:39:01 -0300

    Função anônima

--- funcoes/funcao.js
+++ funcoes/funcao.js
@@ -1,3 +1,6 @@
+console.log('------------------------------------------------------')
+console.log('-----Função: É um bloco de código JavaScript definido uma fez;-----')
+console.log('------------------------------------------------------')
 /*
 Função:
 É um bloco de código JavaScript definido uma fez;
@@ -39,3 +42,54 @@ function retornoUndefined(){
 
 var retorno2 = retornoUndefined();
 console.log("retorno2", retorno2);
+
+
+console.log('------------------------------------------------------')
+console.log('-Função anônima é uma forma definir uma função sem nome, neste formato não pode ser invocada diretamente-')
+console.log('------------------------------------------------------')
+
+/*Função anônima é uma forma definir uma função sem nome, neste formato não pode ser invocada 
+diretamente*/
+/*Como usar:
+- Ideal para armazenar em variáveis, objetos e array;
+- Usar como callback;
+- Manipulador de eventos;
+- IIFE;
+- Retorno de função*/
+/*Vantagens---------------------------------------------------
+- Função anônima não irá poluir o escopo global;
+- Não pode ser chamada diretamente, evitando que qualquer pessoa possa chamar via console do 
+browser;
+- Simplicidade na escrita e proposito de uso;
+*/
+/*Desvantagens-------------------------------------------------
+- Dificulta a reutilização de código;
+- São difíceis de debugar;
+*/
+
+//declarar 
+function ola(){
+    console.log('olá')    
+}
+ola();
+
+var minhaFn = function(){
+    console.log('minha fn')    
+};
+minhaFn();
+
+//window.onload = function(){ console.log('página carregada.') }
+
+var carro = {marca: 'abc', ligar: function(){console.log('carro ligado')}}
+carro.ligar();
+
+(function(){
+    console.log('executando uma IIFE');
+})();
+
+/*setTimeout(function er(){
+    throw new Error('deu um erro :(');
+})*/
+setTimeout(function(){
+    //throw new Error('deu um erro :(');
+})
