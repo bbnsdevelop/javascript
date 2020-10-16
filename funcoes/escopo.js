508134c391b8c21784526704147a708ee2fb7b56
bbnsdevelop <bbnsdevelop@gmail.com>
2020-10-15 22:59:55 -0300

    Aninhamento de funções e encadeamento de escopos

--- funcoes/escopo.js
+++ funcoes/escopo.js
@@ -57,3 +57,47 @@ salaDeAula('Bruno');
 console.log('global aluno1:', aluno1);
 console.log('global aluno2:', aluno2);
 //console.log('global aluno3:', aluno3);//ReferenceError: aluno3 is not defined
+
+
+console.log('------------------------------------------------------')
+console.log('---Aninhamento de funções e encadeamento de escopos---')
+console.log('------------------------------------------------------')
+//Aninhamento de funções e encadeamento de escopos
+
+var produto = 'Produto A';
+
+function orçamento(){
+    var formaPagamento = 'dinheiro';
+    var qtde = 3;
+    var preco = 20;
+    var total = qtde * preco;
+
+    pedido();
+    function pedido(){
+        var numeroPedido = 2345;
+        var entrega = true;
+        var taxaEntrega = 2;
+        totalGeral = entrega == true ? total + taxaEntrega : total;
+
+        notaFiscal();
+        //console.log("numeroNfe", numeroNfe);//ReferenceError: numeroNfe is not defined
+
+        function notaFiscal(){
+            var numeroNfe = 987298;
+
+            console.log('>>>Número pedido:', numeroPedido)
+            console.log('>>>Nota:', numeroNfe);
+            console.log('>>>Produto:', produto);
+            console.log('>>>Qtde:', qtde);
+            console.log('>>>Preço:', preco);
+            console.log('>>>Total:', total);
+            entrega == true ? console.log('>>>Taxa entrega R$:', taxaEntrega) : "";
+            console.log('>>>Total geral R$:', totalGeral);
+
+        }
+
+    }
+}
+
+orçamento();
+//console.log("numeroNfe", numeroNfe);//ReferenceError: numeroNfe is not defined
